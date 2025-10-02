70ca6e96f9473c3ad4b64ab5cfac5093f1e947f0
Anamika Sharma <140599095+anamikasharma7@users.noreply.github.com>
2025-10-02 12:03:33 +0530

    Add files via upload
    basic printing, variables and datatype questions for practice.

---
+++ Homework1/hw1.cpp
@@ -0,0 +1,9 @@
+//Two numbers are given, print their product
+
+#include<iostream>
+using namespace std;
+int main() {
+    int a=3, b=6;
+    cout<<"Product = "<<a*b;
+    return 0;
+}
---
+++ Homework1/hw2.cpp
@@ -0,0 +1,9 @@
+//Two numbers are given a and b, print a-b
+
+#include<iostream>
+using namespace std;
+int main() {
+    int a=3,b =2;
+    cout<<a-b;
+    return 0;
+}
