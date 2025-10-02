3b6f895f6cf449214ed517cd7a444a9b8789483e
Anamika Sharma <140599095+anamikasharma7@users.noreply.github.com>
2025-10-02 12:00:50 +0530

    Add files via upload
    basics printing, cout, variables, datatypes and how to use these in code.

---
+++ Lec 1/basics.cpp
@@ -0,0 +1,10 @@
+//How to print anything in C++
+
+#include<iostream>
+using namespace std;
+int main() {
+    cout<<"Helo World\n";
+    cout<<8+6<<endl;
+    cout<<6;
+    return 0;
+}
---
+++ Lec 1/variablesanddatatype.cpp
@@ -0,0 +1,33 @@
+//Variables and Datatypes in C++
+
+#include<iostream>
+using namespace std;
+int main() {
+
+/*Integer Variable(4 bytes)
+    int a=5, ;
+    cout<<a; //Variable declare and define
+
+    int a=6, b=7,c=3;
+    cout<<a+b;*/
+
+/*Character Variable(1 byte)
+    char c ='d';
+    cout<<c;*/
+
+/*Float Variable(4 byte)
+    float f=6.4;
+    cout<<f;*/
+
+/*Double Variable
+    double d = 3.798;
+    cout<<d;*/
+
+//Boolean Variable
+    bool b= true;
+    cout<<b;
+
+
+
+    return 0;
+}
