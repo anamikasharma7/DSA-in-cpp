4f26f6e077186f3482b3b47609d0c1df7ab5c0b3
Anamika Sharma <140599095+anamikasharma7@users.noreply.github.com>
2025-10-24 07:27:35 +0530

    Add files via upload

---
+++ Lec 4/pattern8.cpp
@@ -0,0 +1,21 @@
+/* a b c d e
+   a b c d e
+   a b c d e
+   a b c d e
+   a b c d e
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    int row,col;
+    for(row=1;row<=5;row=row+1)
+    {
+        for(char col='a';col<='e';col=col+1)
+        {
+            cout<<col<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
